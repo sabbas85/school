cec21e452f0083c495e1dc838e47bdf29e47e972
sarfaraz abbas <sabbas85>
2024-08-28 17:03:52 +0530

    sideMenu

--- src/app/(dashboard)/list/lessons/page.tsx
+++ src/app/(dashboard)/list/lessons/page.tsx
@@ -47,7 +47,7 @@ type Lesson = {
           <div className="flex items-center gap-2">
             <Link href={`/list/students/${item.id}`}>
               <button className="w-7 h-7 flex items-center justify-center rounded-full bg-Sky">
-                <Image src="/view.png" alt="" width={16} height={16} />
+                <Image src="/edit.png" alt="" width={16} height={16} />
               </button>
             </Link>
             {role === "admin" && (
